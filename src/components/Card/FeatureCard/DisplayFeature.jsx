568f8c2e785227f037773aebf46e32db5a8d9499
Fahim Faisaal <fahimfaisal.soikot@gmail.com>
2021-12-14 10:13:15 +0600

    refactor: load image & Detailed feature

--- src/components/Card/FeatureCard/DisplayFeature.jsx
+++ src/components/Card/FeatureCard/DisplayFeature.jsx
@@ -1,13 +1,13 @@
 import features from "../../../variants/features";
-import Button from "../../atoms/Button";
-import Column from "../../atoms/Column";
 import Wrapper from "../../atoms/Wrapper";
+import DetailedFeature from "./DetailedFeature";
+import ImageFeature from "./ImageFeature";
 
 export default function DisplayFeature({ feature }) {
     const selectedFeature = features[feature];
     
     const {
-        image: { path, alt },
+        image,
         heading,
         description,
         list
@@ -17,28 +17,13 @@ export default function DisplayFeature({ feature }) {
         <div className="tab-content">
             <div className="tab-pane fade active show">
                 <Wrapper>
-                    <Column variant="lg6">
-                        <img
-                            className="img-fluid"
-                            src={path}
-                            alt={alt}
-                        />
-                    </Column>
-                    <Column variant="lg6">
-                        <div className="text-container">
-                            <h3>{ heading }</h3>
-                            <p>{ description }</p>
-                            <ul className="list-unstyled li-space-lg">
-                                {list.map((text, index) => (
-                                    <li className="media" key={index}>
-                                        <i className="fas fa-square"></i>
-                                        <div className="media-body">{text}</div>
-                                    </li>
-                                ))}
-                            </ul>
-                            <Button text="LIGHTBOX" />
-                        </div>
-                    </Column>
+                    <ImageFeature image={image} />
+                    <DetailedFeature
+                        heading={heading}
+                        description={description}
+                        list={list}
+                        buttonText="LIGHTBOX"
+                    />
                 </Wrapper>
             </div>
         </div>
